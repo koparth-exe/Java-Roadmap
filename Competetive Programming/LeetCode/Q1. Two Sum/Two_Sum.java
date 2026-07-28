62cb2a605d8ef594cc377c9cfda4fb5dbf2aa152
koparth.exe <parth.korgaonkar07@gmail.com>
2026-07-28 23:32:07 +0530

    Added Lec 6 work

--- Competetive Programming/LeetCode/Q1. Two Sum/Two_Sum.java
+++ Competetive Programming/LeetCode/Q1. Two Sum/Two_Sum.java
@@ -1,6 +1,6 @@
 class Variables
 {
-    int i, j,  x, y, sum, num1, num2;
+    int i, j,  x, y;
 }
 
 class Solution
@@ -8,26 +8,23 @@ class Solution
     public int[] twoSum(int[] nums, int target)
     {
         Variables obj1 = new Variables();
-        int[] array = {2, 7, 11, 15};
-        int myTarget = 9;
-        System.out.println("Target number: "+myTarget);
-        for(obj1.i = 0; obj1.i<array.length; obj1.i++)
-        {
-            obj1.x = array [obj1.i];
-            for(obj1.j = obj1.i + 1; obj1.j < array.length; obj1.j++)
-            {
-                obj1.y = array[obj1.j];
 
-                obj1.sum = obj1.x+obj1.y;
-                if (obj1.sum == target)
-                {
-                    System.out.println(obj1.x+" + "+ obj1.y+" = "+obj1.sum);
-                    return new int[] {obj1.i,obj1.j};
+        for(obj1.i = 0; obj1.i<nums.length; obj1.i++) {
+            obj1.x = nums[obj1.i];
+            for (obj1.j = obj1.i + 1; obj1.j < nums.length; obj1.j++) {
+                obj1.x = nums[obj1.i];
+                obj1.y = nums[obj1.j];
+
+                if (obj1.x + obj1.y == target) {
+                    return new int[]{obj1.i, obj1.j};
                 }
 
             }
         }
-        System.out.println("No such Element present");
         return new int[] {};
     }
 }
+//Runtime
+//68 ms Beats 5.91%
+//Memory
+//46.83 MB Beats 84.97%
