d919595d412f6ba9567191aa3e44c58b262e0f79
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-01 13:09:12 +0530

    Added Lec 7 code no. 1

---
+++ Success Lectures/7. Lec 7/Q2) Multiple Interface/Q1) Multiple Inheritance Code/Main_Cocaine.java
@@ -0,0 +1,26 @@
+interface MU
+{
+    void Maths();
+}
+
+interface PU
+{
+    void Maths();
+}
+
+class Cocaine implements MU, PU
+{
+    public void Maths()
+    {
+        System.out.println("I will teach mental Abacus");
+    }
+}
+
+class Main_Cocaine
+{
+    public static void main (String[] args)
+    {
+        Cocaine obj1 = new Cocaine();
+        obj1.Maths();
+    }
+}
