9a3290fc8260af6655fdb90a01562c9639a16034
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-28 16:50:31 +0530

    Added Lec 7 code no. 4

---
+++ Competetive Programming/LeetCode/Q1464. Maximum Product of Two Elements in an Array/Solution2.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxProduct(int[] nums){
+        Arrays.sort(nums);
+        int i = nums.length - 1;
+        int j = nums.length - 2;
+        return ((nums[i] - 1) * (nums[j] -1));
+    }
+}
+
+// Another better approach
+
+class Solution {
+    public int maxProduct(int[] nums){
+        int max1 = 0;
+        int max2 = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] > max1){
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if (nums[i] > max2){
+                max2 = nums[i];
+            }
+        }
+        return ((max1 - 1) * (max2 -1));
+    }
+}
+//Runtime
+//0
+//ms
+//        Beats
+//100.00%
+//Memory
+//44.17
+//MB
+//        Beats
+//97.04%
---
+++ Success Lectures/7. Lec 7/Q4) Abstract/Purvanchal.java
@@ -0,0 +1,13 @@
+abstract class kaleen
+{
+    void mirzapur()
+    {
+        System.out.println("I am king of Mirzapur");
+    }
+    abstract void johnpur();
+}
+
+abstract class Munna extends kaleen
+{
+
+}
