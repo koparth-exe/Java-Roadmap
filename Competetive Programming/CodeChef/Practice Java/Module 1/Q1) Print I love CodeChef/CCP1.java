c2c4e61155c98a03095f4cc940dc841cbea81d01
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-30 21:12:20 +0530

    Added Lec 7 code no. 4

---
+++ Competetive Programming/CodeChef/Practice Java/Module 1/Q1) Print I love CodeChef/CCP1.java
@@ -0,0 +1,16 @@
+//Print I love CodeChef
+//Print each word of the below-given sentence on a new line.
+//
+//I love Codechef
+//Expected Output:
+//I
+//        love
+//CodeChef
+
+//Code
+class CCP1 {
+    public static void main(String[] args) {
+        // Your code here
+        System.out.println("I\nlove\nCodeChef");
+    }
+}
