c0595a0c236c76cc5ba6b785fdeba2067f3c0470
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-30 23:11:38 +0530

    Added Lec 7 code no. 4

---
+++ Competetive Programming/CodeChef/Practice Java/Module 10/Q1) Print Squares/CCP12.java
@@ -0,0 +1,15 @@
+//
+
+// Code:
+class CCP12
+{
+    public static void main (String[] args)
+    {
+        // your code goes here
+        int i = 1;
+        while (i <= 5){
+            System.out.print(i * i + " ");
+            i++;
+        }
+    }
+}
