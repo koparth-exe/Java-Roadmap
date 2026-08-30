5c2a586ccd009cd8f5ce66c606643d8e86d98118
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-30 21:17:41 +0530

    Added Lec 7 code no. 4

---
+++ Competetive Programming/CodeChef/Practice Java/Module 1/Q2) Print Squares/CCP2.java
@@ -0,0 +1,11 @@
+class CCP2
+{
+    public static void main(String[] args)
+    {
+        // your code goes here
+        for (int i = 1; i <= 5; i++)
+        {
+            System.out.println(i + " - " + i * i);
+        }
+    }
+}
