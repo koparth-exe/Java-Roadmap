bf8629c402b8bb7c048b89c0880617c73ae33243
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-30 21:24:11 +0530

    Added Lec 7 code no. 4

--- Competetive Programming/CodeChef/Practice Java/Module 1/Q2) Print Squares/CCP2.java
+++ Competetive Programming/CodeChef/Practice Java/Module 1/Q2) Print Squares/CCP2.java
@@ -1,3 +1,17 @@
+//Print Squares
+//Write a program to output the squares (using multiplication) of numbers from 1 to 5 on separate lines.
+//
+//        Sample 1:
+//Input
+//        Output
+//
+//1 - 1
+//        2 - 4
+//        3 - 9
+//        4 - 16
+//        5 - 25
+// Code:
+
 class CCP2
 {
     public static void main(String[] args)
