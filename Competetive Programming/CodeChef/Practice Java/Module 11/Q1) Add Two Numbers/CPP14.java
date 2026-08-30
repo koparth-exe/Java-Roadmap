0186ddb9ccb1e5313cf517951bc9d071f634c215
koparth.exe <parth.korgaonkar07@gmail.com>
2026-08-30 23:28:48 +0530

    Added Lec 7 code no. 4

---
+++ Competetive Programming/CodeChef/Practice Java/Module 11/Q1) Add Two Numbers/CPP14.java
@@ -0,0 +1,26 @@
+//
+
+// Code:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class CCP14
+{
+    public static void main (String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int i = 1;
+        while(i <= t)
+        {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            // write your code here
+            System.out.println(a + b);
+            i++;
+        }
+
+    }
+}
