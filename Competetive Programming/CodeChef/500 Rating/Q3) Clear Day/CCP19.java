aa1a41ae22d27da595366fa10728316d967f5e80
koparth.exe <parth.korgaonkar07@gmail.com>
2026-09-05 00:22:55 +0530

    Added CodeChef code

---
+++ Competetive Programming/CodeChef/500 Rating/Q3) Clear Day/CCP19.java
@@ -0,0 +1,18 @@
+//
+
+// Code:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CCP19
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        System.out.println(7-X-Y);
+    }
+}
