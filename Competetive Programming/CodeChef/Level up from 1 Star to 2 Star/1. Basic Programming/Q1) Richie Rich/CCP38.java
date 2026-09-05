5a420c945803bb3db180cd04055837659a5209e6
koparth.exe <parth.korgaonkar07@gmail.com>
2026-09-05 22:05:26 +0530

    Added CodeChef code

---
+++ Competetive Programming/CodeChef/Level up from 1 Star to 2 Star/1. Basic Programming/Q1) Richie Rich/CCP38.java
@@ -0,0 +1,158 @@
+//Richie Rich
+//Chef aims to be the richest person in Chefland by his new restaurant franchise. Currently, his assets are worth
+//A
+//A billion dollars and have no liabilities. He aims to increase his assets by
+//        X
+//X billion dollars per year.
+//
+//        Also, all the richest people in Chefland are not planning to grow and maintain their current worth.
+//
+//To be the richest person in Chefland, he needs to be worth at least
+//        B
+//B billion dollars. How many years will it take Chef to reach his goal if his value increases by
+//X
+//X billion dollars each year?
+//
+//Input
+//The first line contains an integer
+//T
+//T, the number of test cases. Then the test cases follow.
+//Each test case contains a single line of input, three integers
+//        A
+//A,
+//        B
+//                B,
+//                X
+//X.
+//        Output
+//For each test case, output in a single line the answer to the problem.
+//
+//Constraints
+//1
+//        ≤
+//T
+//≤
+//        21
+//
+//        000
+//        1≤T≤21 000
+//        100
+//        ≤
+//A
+//        <
+//                B
+//≤
+//        200
+//        100≤A<B≤200
+//        1
+//        ≤
+//X
+//≤
+//        50
+//        1≤X≤50
+//X
+//X divides
+//B
+//−
+//A
+//B−A
+//        Subtasks
+//Subtask #1 (100 points): Original constraints
+//
+//Sample 1:
+//Input
+//        Output
+//3
+//        100 200 10
+//        111 199 11
+//        190 200 10
+//        10
+//        8
+//        1
+//Explanation:
+//Test Case
+//1
+//        1: Chef needs to increase his worth by
+//200
+//        −
+//        100
+//        =
+//        100
+//        200−100=100 billion dollars and his increment per year being
+//10
+//        10 billion dollars, so it will take him
+//100
+//        10
+//        =
+//        10
+//        10
+//        100
+//        ​
+//        =10 years to do so.
+//
+//        Test Case
+//2
+//        2: Chef needs to increase his worth by
+//199
+//        −
+//        111
+//        =
+//        88
+//        199−111=88 billion dollars and his increment per year being
+//11
+//        11 billion dollars, so it will take him
+//88
+//        11
+//        =
+//        8
+//        11
+//        88
+//        ​
+//        =8 years to do so.
+//
+//        Test Case
+//3
+//        3: Chef needs to increase his worth by
+//200
+//        −
+//        190
+//        =
+//        10
+//        200−190=10 billion dollars and his increment per year being
+//10
+//        10 billion dollars, so it will take him
+//10
+//        10
+//        =
+//        1
+//        10
+//        10
+//        ​
+//        =1 year to do so.
+
+// Code:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CCP38
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        int i = 1;
+        while (i <= T)
+        {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int X = sc.nextInt();
+
+            System.out.println((B - A) / X);
+            i++;
+        }
+
+    }
+}
