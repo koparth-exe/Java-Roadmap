61f794889b9a61e7b5da8ff6096e7a5003543cde
koparth.exe <parth.korgaonkar07@gmail.com>
2026-09-09 00:18:42 +0530

    Added CodeChef code

---
+++ Competetive Programming/CodeChef/500 Rating/Q38) Chef and Donation/CCP58.java
@@ -0,0 +1,142 @@
+//Chef and Donation
+//In a certain month, Chef earned
+//        X
+//X rupees while Chefina earned
+//Y
+//Y rupees such that
+//Y
+//>
+//X
+//Y>X.
+//Since they want to end up with exactly the same amount, they decide to donate the difference between their income to a charity.
+//
+//Find the amount they donate in the month.
+//
+//Input Format
+//The first line of input will contain a single integer
+//T
+//T, denoting the number of test cases.
+//Each test case consists of two space-separated integers
+//X
+//X and
+//Y
+//Y — the income of Chef and Chefina in a month, respectively.
+//        Output Format
+//For each test case, output on a new line, the amount they donate in the month.
+//
+//        Constraints
+//1
+//        ≤
+//T
+//≤
+//        100
+//        1≤T≤100
+//        1
+//        ≤
+//X
+//        <
+//                Y
+//≤
+//        10
+//        1≤X<Y≤10
+//Sample 1:
+//Input
+//        Output
+//4
+//        1 3
+//        2 5
+//        4 5
+//        2 10
+//        2
+//        3
+//        1
+//        8
+//Explanation:
+//Test case
+//        1
+//        1: Chef earns
+//1
+//        1 rupees while Chefina earns
+//3
+//        3 rupees. The difference between their income is
+//3
+//        −
+//        1
+//        =
+//        2
+//        3−1=2. Thus, they donate
+//2
+//        2 rupees to charity.
+//
+//        Test case
+//        2
+//        2: Chef earns
+//2
+//        2 rupees while Chefina earns
+//5
+//        5 rupees. The difference between their income is
+//5
+//        −
+//        2
+//        =
+//        3
+//        5−2=3. Thus, they donate
+//3
+//        3 rupees to charity.
+//
+//        Test case
+//        3
+//        3: Chef earns
+//4
+//        4 rupees while Chefina earns
+//5
+//        5 rupees. The difference between their income is
+//5
+//        −
+//        4
+//        =
+//        1
+//        5−4=1. Thus, they donate
+//1
+//        1 rupees to charity.
+//
+//        Test case
+//        4
+//        4: Chef earns
+//2
+//        2 rupees while Chefina earns
+//10
+//        10 rupees. The difference between their income is
+//10
+//        −
+//        2
+//        =
+//        8
+//        10−2=8. Thus, they donate
+//8
+//        8 rupees to charity.
+
+// Code:
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CCP58
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 1; i <= T; i++)
+        {
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+            if (Y > X)
+            {
+                System.out.println(Y - X);
+            }
+        }
+    }
+}
